a85d568402ba9658af54284529d1d6a5a53d3ee1
eriku66 <eriku.hashimoto@gmail.com>
2024-11-26 23:54:52 +0900

    feat: 🎸 connect server and client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,13 @@
+use std::net::UdpSocket;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter your name:");
+
+    let mut name = String::new();
+    std::io::stdin().read_line(&mut name).unwrap();
+
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    socket
+        .send_to(&name.bytes().collect::<Vec<_>>(), "127.0.0.1:8080")
+        .unwrap();
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,31 @@
+use std::net::UdpSocket;
+
+fn create_socket() -> std::io::Result<UdpSocket> {
+    UdpSocket::bind("127.0.0.1:8080")
+}
+
+fn handle_socket(socket: UdpSocket) {
+    let mut buf = [0; 4096];
+    match socket.recv_from(&mut buf) {
+        Ok((received, client_socket_addr)) => {
+            println!("Received {} bytes", received);
+            println!("Client: {:?}", client_socket_addr);
+            println!(
+                "User name: {:?}",
+                String::from_utf8_lossy(&buf[..received]).trim()
+            );
+        }
+        Err(e) => eprintln!("Failed to receive message: {}", e),
+    }
+}
+
+fn start_server() {
+    match create_socket() {
+        Ok(socket) => handle_socket(socket),
+        Err(e) => eprintln!("Failed to create socket: {}", e),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    start_server();
 }
