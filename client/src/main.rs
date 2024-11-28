8ee9d86e4f7f68ac509d093f6abfb8aa99de922a
eriku66 <eriku.hashimoto@gmail.com>
2024-11-28 23:15:20 +0900

    feat: 🎸 add user name length validation and constant

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,11 +3,20 @@ use std::net::UdpSocket;
 fn main() {
     println!("Please enter your name:");
 
-    let mut name = String::new();
-    std::io::stdin().read_line(&mut name).unwrap();
+    let mut user_name = String::new();
+    std::io::stdin().read_line(&mut user_name).unwrap();
+
+    if user_name.as_bytes().len() > shared::MAX_USER_NAME_SIZE_BYTES {
+        eprintln!(
+            "User name must be less than or equal to {} bytes",
+            shared::MAX_USER_NAME_SIZE_BYTES
+        );
+
+        return;
+    }
 
     let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
     socket
-        .send_to(name.as_bytes(), shared::SERVER_ADDR)
+        .send_to(user_name.as_bytes(), shared::SERVER_ADDR)
         .unwrap();
 }
--- shared/src/consts.rs
+++ shared/src/consts.rs
@@ -1,2 +1,3 @@
 pub const SERVER_ADDR: &str = "127.0.0.1:8080";
 pub const MAX_MESSAGE_SIZE_BYTES: usize = 4096;
+pub const MAX_USER_NAME_SIZE_BYTES: usize = 255;
