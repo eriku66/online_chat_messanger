8d3541c37e6ec9b016ffd2dc726f1608d2c22be0
eriku66 <eriku.hashimoto@gmail.com>
2024-11-29 23:22:18 +0900

    refactor: 💡 Change return type of main to Result<()>

--- client/src/main.rs
+++ client/src/main.rs
@@ -15,14 +15,13 @@ fn get_user_name() -> Result<UserName> {
     UserName::new(prompt_user_name())
 }
 
-fn main() {
-    let user_name = get_user_name().unwrap_or_else(|e| {
-        eprintln!("{}", e);
-        std::process::exit(1);
-    });
+fn main() -> Result<()> {
+    let user_name = get_user_name()?;
 
     let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
     socket
         .send_to(user_name.value().as_bytes(), shared::SERVER_ADDR)
         .unwrap();
+
+    Ok(())
 }
