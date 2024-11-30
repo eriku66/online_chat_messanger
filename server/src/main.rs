b87cecf2b461d37cd42bf6e38d7e7a7b872e172e
eriku66 <eriku.hashimoto@gmail.com>
2024-11-30 22:30:32 +0900

    feat: 🎸 Add anyhow to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,31 +1,34 @@
+use anyhow::{Context, Result};
 use std::net::UdpSocket;
 
 fn create_socket() -> std::io::Result<UdpSocket> {
     UdpSocket::bind(shared::SERVER_ADDR)
 }
 
-fn handle_socket(socket: UdpSocket) {
+fn handle_socket(socket: UdpSocket) -> Result<()> {
     let mut buf = [0; shared::MAX_MESSAGE_SIZE_BYTES];
-    match socket.recv_from(&mut buf) {
-        Ok((received, client_socket_addr)) => {
-            println!("Received {} bytes", received);
-            println!("Client: {:?}", client_socket_addr);
-            println!(
-                "User name: {:?}",
-                String::from_utf8_lossy(&buf[..received]).trim()
-            );
-        }
-        Err(e) => eprintln!("Failed to receive message: {}", e),
-    }
+    let (received, client_socket_addr) = socket
+        .recv_from(&mut buf)
+        .context("Failed to receive message")?;
+    println!("Received {} bytes", received);
+    println!("Client: {:?}", client_socket_addr);
+    println!(
+        "User name: {:?}",
+        String::from_utf8_lossy(&buf[..received]).trim()
+    );
+
+    Ok(())
 }
 
-fn start_server() {
-    match create_socket() {
-        Ok(socket) => handle_socket(socket),
-        Err(e) => eprintln!("Failed to create socket: {}", e),
-    }
+fn start_server() -> Result<()> {
+    let socket = create_socket()?;
+    handle_socket(socket)?;
+
+    Ok(())
 }
 
-fn main() {
-    start_server();
+fn main() -> Result<()> {
+    start_server()?;
+
+    Ok(())
 }
