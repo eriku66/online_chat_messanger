169660e64b6df2d00fd3093954168f7f6310d882
eriku66 <eriku.hashimoto@gmail.com>
2024-11-30 22:57:10 +0900

    refactor: 💡 Inline user name creation in main function

--- client/src/main.rs
+++ client/src/main.rs
@@ -11,12 +11,8 @@ fn prompt_user_name() -> String {
     user_name
 }
 
-fn get_user_name() -> Result<UserName> {
-    UserName::new(prompt_user_name())
-}
-
 fn main() -> Result<()> {
-    let user_name = get_user_name()?;
+    let user_name = UserName::new(prompt_user_name())?;
 
     let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
     socket
