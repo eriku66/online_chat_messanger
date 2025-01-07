22c79461916f34129db3f566b63368aa24a915bf
eriku66 <eriku.hashimoto@gmail.com>
2025-01-07 23:47:15 +0900

    feat: 🎸 update UdpMessagePacket to include chat room name and user token for enhanced message handling

--- shared/src/protocol/udp_message_packet.rs
+++ shared/src/protocol/udp_message_packet.rs
@@ -1,35 +1,56 @@
-use crate::{Message, UserName};
+use crate::{ChatRoomName, Message, UserToken};
 use anyhow::Result;
 
 #[derive(Debug)]
 pub struct UdpMessagePacket {
-    pub user_name: UserName,
+    pub chat_room_name: ChatRoomName,
+    pub user_token: UserToken,
     pub message: Message,
 }
 
 impl UdpMessagePacket {
-    pub fn new(user_name: UserName, message: Message) -> Self {
-        Self { user_name, message }
+    pub fn new(chat_room_name: ChatRoomName, user_token: UserToken, message: Message) -> Self {
+        Self {
+            chat_room_name,
+            user_token,
+            message,
+        }
     }
 
     pub fn generate_packet(&self) -> Vec<u8> {
         let mut packet = Vec::new();
 
-        packet.push(self.user_name.length() as u8);
-        packet.extend_from_slice(self.user_name.value().as_bytes());
+        packet.push(self.chat_room_name.length() as u8);
+        packet.extend_from_slice(self.chat_room_name.value().as_bytes());
         packet.extend_from_slice(self.message.value().as_bytes());
 
         packet
     }
 
     pub fn from_packet(packet: &[u8]) -> Result<Self> {
-        let user_name_length = u8::from_be_bytes([packet[0]]) as usize;
-        let user_name_and_message = String::from_utf8_lossy(&packet[1..]).to_string();
-        let (user_name, message) = user_name_and_message.split_at(user_name_length);
+        let chat_room_name_length = u8::from_be_bytes([packet[0]]) as usize;
+        let token_size_length = u8::from_be_bytes([packet[1]]) as usize;
+
+        let mut read_index = 2;
+
+        let chat_room_name_str =
+            String::from_utf8_lossy(&packet[read_index..(read_index + chat_room_name_length)])
+                .to_string();
+
+        read_index += chat_room_name_length;
+
+        let user_token =
+            String::from_utf8_lossy(&packet[read_index..(read_index + token_size_length)])
+                .to_string();
+
+        read_index += token_size_length;
+
+        let message_str = String::from_utf8_lossy(&packet[read_index..]).to_string();
 
         Ok(Self {
-            user_name: UserName::new(user_name.to_string())?,
-            message: Message::new(message.to_string())?,
+            chat_room_name: ChatRoomName::new(chat_room_name_str)?,
+            user_token: UserToken::new(user_token),
+            message: Message::new(message_str)?,
         })
     }
 }
